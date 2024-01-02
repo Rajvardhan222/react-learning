9312487954de2b5ac943f6263fe6ea7f7a8746be
Rajvardhan222 <146507318+Rajvardhan222@users.noreply.github.com>
2024-01-02 12:42:43 +0000

    .

---
+++ currencycalc/src/hooks/currencyonfo.js
@@ -0,0 +1,16 @@
+import {useState,useEffect} from "react"
+let useCurrencyInfo(currency)=>{
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+    let [data,setData] = useState({})
+    useEffect(()=>{
+        fetch(url)
+        .than((res)=>{
+           return res.json();
+        })
+        .than((res)=> res[setData] )
+
+    },[currency])
+    console.log(data);
+    return data;
+}
+export default useCurrencyInfo;
